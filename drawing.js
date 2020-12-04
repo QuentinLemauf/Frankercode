4ffb1c76ed423c4bf6ff16320e2f91506532d568
la_nuit_des_as <75462327+LaNuitDesAs@users.noreply.github.com>
2020-12-04 04:47:54 +0100

    Add files via upload

--- drawing.js
+++ drawing.js
@@ -2,15 +2,14 @@
 var cnv,ctx,X,Y,D,N,TIT,CLR0,CLR1,CC=[],start;
 
     function Step() {
-      document.title="Game of life ('"+TIT+"' step: "+(++N)+")";
-      //console.log(TIT);
+      document.title="jeu de la vie ('"+TIT+"' etape(s): "+(++N)+")";//afficher le nom du motif utilisé initialement
       X1=X-1,Y1=Y-1,pp=[],aa=CC.slice(0,X),Z=X;
-      d=D+1; y=1; do { // évite les bords
+      d=D+1; y=1; do { // esquive les bords
         z=Z; Z+=X;
         x=0; do {pp[x]=aa[x]; aa[x]=CC[z+x];} while (++x<X);
-        x=1; do { // évite les bords
+        x=1; do { // esquive les bords
           n=pp[x-1]+pp[x]+pp[x+1]+aa[x-1]+aa[x+1]+CC[Z+x-1]+CC[Z+x]+CC[Z+x+1];
-          if (n==2) continue; // ne traite que les cellules modifiées
+          if (n==2) continue; // traite seulement les cellules modifiés
           if (CC[c=z+x]) {
             if (n!=3) {CC[c]=0; ctx.fillStyle=CLR0; ctx.fillRect(1+x*d,1+y*d,D,D);}
           } else {
@@ -20,34 +19,30 @@ var cnv,ctx,X,Y,D,N,TIT,CLR0,CLR1,CC=[],start;
       } while(++y<Y1);
     }
 
-    function loop_main(){
+    function loop_main(){//permet de faire renouveler le motif
       FRAME_RATE = 60 // fps;
       const loop = window.setInterval(function(){
       Step();
       }, 4000/FRAME_RATE)
       }
-    
-    //function Key(ev) {
-     // if ((k=ev.keyCode)==32) {/*' '*/ if(!start){start=1;loop_main()}; return;}
-     // if ((k>=48)&&(k<=57)) {/*'0'...'9'*/ FctIni(k-48); N=0; Dims();
-     //   document.title="Game of life ('"+TIT+"' step: 0)"; return;}
-     // alert("Jeu de la vie: Aide [Help]\n'  ':\tavancer d'une étape [next]"
-     //   +"\n'S','B':\tdimension des cellules [Smaller or Bigger]"
-     //   +"\n'0'...'9':\tno. fonction d'initialisation (selon 'FctIni.js')");
-    //}
-    function Click(ev)  {
+
+    function Click(ev)  {//permet de rajouter en temps réel une cellule dans le jeu
       r=cnv.getBoundingClientRect();
       d=D+1; x=(ev.clientX-r.left)/d|0; y=(ev.clientY-r.top)/d|0; z=x+y*X;
       ctx.fillStyle=(CC[z]=1-CC[z])?CLR1:CLR0; ctx.fillRect(1+x*d,1+y*d,D,D);
     }
-    function Dims() {
+
+
+    function Dims() {// gere les dimmensions du canva
       cnv.width=DX=window.innerWidth; cnv.height=DY=window.innerHeight ;
       ctx.fillStyle=CLR0; ctx.fillRect(0,0,DX,DY); ctx.fillStyle=CLR1;
       d=D+1; z=0; y=0; do {
         x=0; do if(CC[z++]) ctx.fillRect(1+x*d,1+y*d,D,D); while (++x<X);
       } while (++y<Y);
     }
-    function Ini() {
+
+
+    function Ini() {// fonction appeler par la page html
       loop_main();
       cnv=document.getElementById('Can');
       cnv.addEventListener('click',Click,false);
---
+++ pattern.js
@@ -0,0 +1,60 @@
+function FctIni(n) { // appelée par index.html
+  X= window.innerWidth  || document.documentElement.clientWidth || document.body.clientWidth; // taille du canva en X
+  Y= window.innerHeight || document.documentElement.clientHeight|| document.body.clientHeight; // taille du canva en Y
+  D=16;  // 4 ou 8 ou 16
+  Y=Y/D;
+  X=X/D;
+  CLR0='#000000'; // couleur background
+  CLR1='#FFFFFF'; // couleur cellule
+  switch (n) { // Initialisation de plusieurs figures connues
+
+
+  case 0:
+    TIT="Aléatoire";Clear();
+    n=X*Y/3; do CC[Math.floor(X*Y*Math.random())]=1; while (--n>0);
+    return;
+
+
+  case 1:
+    TIT="Clown";Clear();
+    i=Math.floor((Y/2))*X+Math.floor(X/2);
+    CC[i-1]=CC[i]=CC[i+1]=CC[(i-1)+X]=CC[(i+1)+X]=CC[(i-1)+2*X]=CC[(i+1)+2*X]=1;
+    return;
+
+
+  case 2:
+    TIT="Ligne"; Clear();
+    i=X*Math.floor(Y/2); m=i+X; do CC[i]=1; while (++i<m);
+    return;
+
+
+  case 3:
+    TIT="Gosper glider gun";
+    Clear();
+    i=X; CC[i+25]=1;
+    i+=X; CC[i+23]=CC[i+25]=1;
+    i+=X; CC[i+13]=CC[i+14]=CC[i+21]=CC[i+22]=CC[i+35]=CC[i+36]=1;
+    i+=X; CC[i+12]=CC[i+16]=CC[i+21]=CC[i+22]=CC[i+35]=CC[i+36]=1;
+    i+=X; CC[i+1]=CC[i+2]=CC[i+11]=CC[i+17]=CC[i+21]=CC[i+22]=1;
+    i+=X; CC[i+1]=CC[i+2]=CC[i+11]=CC[i+15]=CC[i+17]=CC[i+18]=CC[i+23]=CC[i+25]=1;
+    i+=X; CC[i+11]=CC[i+17]=CC[i+25]=1;
+    i+=X; CC[i+12]=CC[i+16]=1;
+    i+=X; CC[i+13]=CC[i+14]=1;
+   return;
+
+  case 4:
+    TIT="Thunderbird";  Clear();
+    i=12*X+Math.floor(X/2);
+    CC[i-1]=CC[i]=CC[i+1]=CC[i+2*X]=CC[i+3*X]=CC[i+4*X]=1;
+    return;
+
+  case 5:
+    TIT="Staros"; Clear();
+    i=Math.floor((Y/2))*X+Math.floor(X/2)-2*X; CC[i-2]=CC[i]=CC[i+2]=1;
+    i=i+X; CC[i-2]=CC[i+2]=1;
+    i=i+X; CC[i-2]=CC[i+2]=1;
+    i=i+X; CC[i-2]=CC[i+2]=1;
+    i=i+X; CC[i-2]=CC[i]=CC[i+2]=1;
+  }
+}
+function Clear() {i=X*Y; do CC[--i]=0; while (i>0);}
